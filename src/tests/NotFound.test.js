5a6abfaf2fc0330ff6d8edcb5ff2cc9124096083
CamilaPaiz <camilapaiz@hotmail.com>
2022-09-19 19:12:21 -0300

    requisito 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { NotFound } from '../pages';
+
 describe('testando component Not Found', () =>{
   test('Teste se contém h2 com o texto Page requested not found', () => {
     renderWithRouter(<NotFound />);
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('testando componente Pokedex', () => {
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: /all/i });
+    expect(buttonAll).toBeInTheDocument();
+    const buttonElectric = screen.getByRole('button', { name: /electric/i });
+    expect(buttonElectric).toBeInTheDocument();
+    const buttonFire = screen.getByRole('button', { name: /fire/i });
+    expect(buttonFire).toBeInTheDocument();
+    const buttonBug = screen.getByRole('button', { name: /bug/i });
+    expect(buttonBug).toBeInTheDocument();
+    const buttonPoison = screen.getByRole('button', { name: /poison/i });
+    expect(buttonPoison).toBeInTheDocument();
+    const buttonPsychic = screen.getByRole('button', { name: /psychic/i });
+    expect(buttonPsychic).toBeInTheDocument();
+    const buttonNormal = screen.getByRole('button', { name: /normal/i });
+    expect(buttonNormal).toBeInTheDocument();
+    const buttonDragon = screen.getByRole('button', { name: /dragon/i });
+    expect(buttonDragon).toBeInTheDocument();
+  });
+
+  test('Botões de filtragem por tipo possuem o data-testid=pokemon-type-button', () => {
+    renderWithRouter(<App />);
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    expect(buttons[0]).toHaveAttribute('data-testid', 'pokemon-type-button');
+  });
+
+  test('É possível clicar no botão de filtragem All', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByText(/all/i);
+    userEvent.click(buttonAll);
+    expect(buttonAll).toBeInTheDocument();
+});
+});
